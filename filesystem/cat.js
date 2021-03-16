1a0dd124c38a4d99da15105325e049e077e7d902
mihailstamenov <badman_tings@yahoo.com>
2021-03-16 03:48:49 +0200

    Fix typos, rephrase for clarity

--- filesystem/cat.js
+++ filesystem/cat.js
@@ -3,7 +3,7 @@
 'use strict';
 require('fs').createReadStream(process.argv[2]).pipe(process.stdout);
 
-// Because the first line starts with #!, you can execute this program directly inUnix-like systems. You don’t need to pass it into the node program (althoughyou still can).
+// Because the first line starts with #!, you can execute this program directly in Unix-like systems. You don’t need to pass it into the node program (although you still can).
 
-// Use chmod +x cat.js to make it executable:
+// Use chmod +x cat.js in terminal to make it executable:
 // Then, to run it, send the name of the chosen file as an additional argument ./cat.js target.txt
